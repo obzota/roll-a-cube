4128d4c037f51ccd54acb83f88f45f55f6b6ff57
obzota <rey.rjf@gmail.com>
2017-07-24 17:18:58 +0200

    Roll around with animator

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Animator _anim;
+	private float _horizontalMovement = 0;
+	private float _verticalMovement = 0;
+
+	private int _hashLeft = Animator.StringToHash("Left");
+	private int _hashRight = Animator.StringToHash("Right");
+	private int _hashUp = Animator.StringToHash ("Up");
+	private int _hashDown = Animator.StringToHash ("Down");
+
+	private float _gain = 100;
+
+	// Use this for initialization
+	void Start () {
+		_anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_horizontalMovement = Input.GetAxis ("Horizontal") * _gain;
+		_verticalMovement = Input.GetAxis ("Vertical") * _gain;
+
+		_anim.ResetTrigger (_hashLeft);
+		_anim.ResetTrigger (_hashRight);
+		_anim.ResetTrigger (_hashUp);
+		_anim.ResetTrigger (_hashDown);
+
+		if (_horizontalMovement < -0.1f) {
+			_anim.SetTrigger (_hashLeft);
+		} else if (_horizontalMovement > 0.1f) {
+			_anim.SetTrigger (_hashRight);
+		} else if (_verticalMovement > 0.1f) {
+			_anim.SetTrigger (_hashUp);
+		} else if (_verticalMovement < -0.1f) {
+			_anim.SetTrigger (_hashDown);
+		}
+	}
+
+}
+
